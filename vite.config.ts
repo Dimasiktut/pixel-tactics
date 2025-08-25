29e4c74a814f605611fb1825ba83be6c06f332ec
Fallxan <50364597+Dimasiktut@users.noreply.github.com>
2025-08-25 15:17:13 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
+    base: '/pixel-tactics/', // замени на название твоего репо
     const env = loadEnv(mode, '.', '');
     return {
       define: {
